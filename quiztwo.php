6f5fbb3c0547a06fc9583b6c8a1c1feba1bc1982
Peridottemmy <seyytemmy@gmail.com>
2018-10-11 18:01:36 +0100

    second commit

---
+++ quiztwo.php
@@ -0,0 +1,16 @@
+<?php
+# three different variable a,b,c;
+$a = 4;
+$b = 2;
+$c = 12;
+# multiplication of the first and third variable;
+$d = $a*$b . "\n";
+# subraction of the first variable from the second variable;
+$e = $b-$a . "\n";
+# Division of the third variable by the second variable;
+$f = $c/$b . "\n";
+# The product of a and be is 8, the subraction of a from b is -2 and the division of c by is 6;
+echo "$d<br>";
+echo "$e<br>";
+echo "$f<br>";
+?>
