ab9b2eb224084d13cd594fce083731ee60f8e99c
Peridottemmy <seyytemmy@gmail.com>
2018-10-17 19:19:52 +0100

    fifth commit

---
+++ quizfive.php
@@ -0,0 +1,7 @@
+<?php
+$name = array("seyi", "funke", "tolu");
+$hobbies = array("reading", "talking","coding");
+echo "My name is " . $name[0] . ", I love " . $hobbies[0] . "<br>";
+echo "My name is " . $name[1] . ", I love " . $hobbies[1] . "<br>";
+echo "My name is " . $name[2] . ", I love " . $hobbies[2] . "<br>";
+?>
