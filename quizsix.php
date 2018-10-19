10e6194bfa07db65822cd13d02465dcf443e5a9d
Peridottemmy <seyytemmy@gmail.com>
2018-10-19 12:38:51 +0100

    sixth commit

---
+++ quizsix.php
@@ -0,0 +1,12 @@
+<?php
+function addfunction($x, $y) {
+    $sum  = $x + $y;
+    echo   "The Sum x and y is : $sum <br>";
+}
+addfunction(10,5);
+function  multiplyfunction($x, $y) {
+    $multiply = $x * $b;
+    echo "The product of x and y is : $multiply <br>";
+}
+multiplyfunction(10,5);
+?>
